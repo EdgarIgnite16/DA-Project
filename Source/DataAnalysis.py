4294f74bd33fb1faab093d9cc8501327547c6188
Edgar <edgarignite1704@gmail.com>
2023-12-01 09:22:29 +0700

    update LNR-2

--- Source/DataAnalysis.py
+++ Source/DataAnalysis.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt_test
 import matplotlib.pyplot as plt_lnr2
 import matplotlib.pyplot as plt_lnr1
 import matplotlib.pyplot as plt_svr
 import matplotlib.pyplot as plt_all
-import seaborn as sns_test
 import seaborn as sns_lnr2
 import seaborn as sns_lnr1
 import seaborn as sns_svr
@@ -21,15 +19,16 @@ from sklearn import metrics
 
 # hàm chính
 def Analysis(dataframe, originDF):
-    # Hiển thị thông tin dataframe thông qua biểu đồ
+    # EDA
     # highestProfitMargins(dataframe)
     # productLagestAndSmallest(dataframe)
     # pricesVaryWithinCategories_WS(dataframe)
     # pricesVaryWithinCategories_Re(dataframe)
+    # ShowValuebaleBetweenCustomerRank(originDF
     # describeDF(dataframe) # Mô tả thông số thống kê
-    # ShowValuebaleBetweenCustomerRank(originDF)
 
-    # Tiến hành phân tích tập dữ liệu:
+    print(dataframe)
+    # Data Analyst
     DA(dataframe)
 
 # ==================================================== #
@@ -117,11 +116,6 @@ def pricesVaryWithinCategories_Re(df):
 
     fig.show()
 
-# Mô tả dữ liệu thống kê
-def describeDF(df):
-    print(df['Wholesale Price'].describe())
-    print(df['Retail Price'].describe())
-
 # Hiển thị điểm khác biệt giữa các rank thành viên người dùng
 def ShowValuebaleBetweenCustomerRank(df):
     dfDate = df
@@ -159,6 +153,11 @@ def ShowValuebaleBetweenCustomerRank(df):
 
     # Kết luận: Không có lợi thế gì khi trở thành khách hàng Bạch kim liên quan đến việc giao hàng trễ
 
+# Mô tả dữ liệu thống kê
+def describeDF(df):
+    print(df['Wholesale Price'].describe())
+    print(df['Retail Price'].describe())
+
 # ==================================================== #
 # Phân tích dữ liệu
 def DA(df):
@@ -173,7 +172,6 @@ def DA(df):
     # Bắt đầu huấn luyện mô hình
     lm2 = LinearRegression()
     lm2.fit(x_train_lnr2,y_train_lnr2) # x_train, y_train
-    print(lm2.coef_) # in kết quả huấn luyện
 
     # Dự đoán dữ liệu thử nghiệm
     predictions_lnr2 = lm2.predict(x_test_lnr2)
@@ -216,8 +214,7 @@ def DA(df):
     # Bắt đầu huấn luyện mô hình
     lm1 = LinearRegression()
     lm1.fit(x_train_lnr1, y_train_lnr1)
-    print(lm1.coef_)
-
+    
     # Dự đoán dữ liệu thử nghiệm
     predictions_lnr1 = lm1.predict(x_test_lnr1)
     sns_lnr1.scatterplot(x=y_test_lnr1, y=predictions_lnr1)
@@ -245,104 +242,104 @@ def DA(df):
     cdf_lnr1 = pd.DataFrame(lm1.coef_, x_lnr1.columns, columns=["Coefficient"])
     print(cdf_lnr1)
 
-    # ==================================================================== # SVR
-    print("Thực hiện SVR")
-    y_svr = df["Retail Price"] # Biến phụ thuộc
-    x_svr = df[['Wholesale Price', 'Total Sold']] # Biến độc lập
-    x_train_svr, x_test_svr, y_train_svr, y_test_svr = train_test_split(x_svr, y_svr, test_size=0.3, random_state=101)
+    # # ==================================================================== # SVR
+    # print("Thực hiện SVR")
+    # y_svr = df["Retail Price"] # Biến phụ thuộc
+    # x_svr = df[['Wholesale Price', 'Total Sold']] # Biến độc lập
+    # x_train_svr, x_test_svr, y_train_svr, y_test_svr = train_test_split(x_svr, y_svr, test_size=0.3, random_state=101)
 
-    # Gọi thư viện truyền tham số mô hình
-    scaler = StandardScaler()
-    x_train_svr = scaler.fit_transform(x_train_svr)
-    x_test_svr = scaler.transform(x_test_svr)
+    # # Gọi thư viện truyền tham số mô hình
+    # scaler = StandardScaler()
+    # x_train_svr = scaler.fit_transform(x_train_svr)
+    # x_test_svr = scaler.transform(x_test_svr)
 
-    # Xác định các tham số huấn luyện
-    svr_rbf = SVR(kernel="rbf", C=100, gamma=0.1, epsilon=0.1)
-    svr_lin = SVR(kernel="linear", C=100, gamma="auto")
-    svr_poly = SVR(kernel="poly", C=100, gamma="auto", degree=3, epsilon=0.1, coef0=1)
+    # # Xác định các tham số huấn luyện
+    # svr_rbf = SVR(kernel="rbf", C=100, gamma=0.1, epsilon=0.1)
+    # svr_lin = SVR(kernel="linear", C=100, gamma="auto")
+    # svr_poly = SVR(kernel="poly", C=100, gamma="auto", degree=3, epsilon=0.1, coef0=1)
 
-    # Thực nghiệm huấn luyện mô hình
-    svr_rbf.fit(x_train_svr, y_train_svr) #RBF
-    predictions_rbf = svr_rbf.predict(x_test_svr)
+    # # Thực nghiệm huấn luyện mô hình
+    # svr_rbf.fit(x_train_svr, y_train_svr) #RBF
+    # predictions_rbf = svr_rbf.predict(x_test_svr)
 
-    svr_lin.fit(x_train_svr, y_train_svr) #LIN
-    predictions_lin = svr_lin.predict(x_test_svr)
+    # svr_lin.fit(x_train_svr, y_train_svr) #LIN
+    # predictions_lin = svr_lin.predict(x_test_svr)
 
-    svr_poly.fit(x_train_svr, y_train_svr) #POLY
-    predictions_poly = svr_poly.predict(x_test_svr)
+    # svr_poly.fit(x_train_svr, y_train_svr) #POLY
+    # predictions_poly = svr_poly.predict(x_test_svr)
 
-    # Hiển thị tập dữ liệu vừa huấn luyện thành dạng mô hình lên màn hình
-    fig, axes = plt_svr.subplots(1, 3, figsize=(12, 8), sharey=True)
-    fig.suptitle('SVR')
-    fig.supylabel('Predicted Values')
-    fig.supxlabel('Y Test (True Values)')
+    # # Hiển thị tập dữ liệu vừa huấn luyện thành dạng mô hình lên màn hình
+    # fig, axes = plt_svr.subplots(1, 3, figsize=(12, 8), sharey=True)
+    # fig.suptitle('SVR')
+    # fig.supylabel('Predicted Values')
+    # fig.supxlabel('Y Test (True Values)')
 
-    # RBF
-    sns_svr.scatterplot(ax=axes[0], x=y_test_svr, y=predictions_rbf)
-    axes[0].set_title('RBF')
+    # # RBF
+    # sns_svr.scatterplot(ax=axes[0], x=y_test_svr, y=predictions_rbf)
+    # axes[0].set_title('RBF')
 
-    # Linear
-    sns_svr.scatterplot(ax=axes[1], x=y_test_svr, y=predictions_lin)
-    axes[1].set_title('Linear')
+    # # Linear
+    # sns_svr.scatterplot(ax=axes[1], x=y_test_svr, y=predictions_lin)
+    # axes[1].set_title('Linear')
 
-    # Poly
-    sns_svr.scatterplot(ax=axes[2], x=y_test_svr, y=predictions_poly)
-    axes[2].set_title('Poly')
+    # # Poly
+    # sns_svr.scatterplot(ax=axes[2], x=y_test_svr, y=predictions_poly)
+    # axes[2].set_title('Poly')
 
-    plt_svr.show() # Hiển thị
+    # plt_svr.show() # Hiển thị
 
-    # Đánh giá điểm số phương sai (điểm số hồi quy)
-    r2_rbf = metrics.explained_variance_score(y_test_svr, predictions_rbf)
-    r2_lin = metrics.explained_variance_score(y_test_svr, predictions_lin)
-    r2_poly = metrics.explained_variance_score(y_test_svr, predictions_poly)
-    print(r2_rbf, r2_lin, r2_poly)
+    # # Đánh giá điểm số phương sai (điểm số hồi quy)
+    # r2_rbf = metrics.explained_variance_score(y_test_svr, predictions_rbf)
+    # r2_lin = metrics.explained_variance_score(y_test_svr, predictions_lin)
+    # r2_poly = metrics.explained_variance_score(y_test_svr, predictions_poly)
+    # print(r2_rbf, r2_lin, r2_poly)
 
-    # ==================================================================== # Giả định
-    print("Thực hiện giả định")
-    # Dự đoán giá bán lẻ của một mặt hàng có giá bán sỉ là 199,99 và tổng lượng bán dự kiến ​​là 500 trong 5 năm
-    new_product2v = [[199.99, 500]]
-    new_product1v = [[199.99]]
-    new_product2v_svr = scaler.transform([[199.99, 500]])
+    # # ==================================================================== # Giả định
+    # print("Thực hiện giả định")
+    # # Dự đoán giá bán lẻ của một mặt hàng có giá bán sỉ là 199,99 và tổng lượng bán dự kiến ​​là 500 trong 5 năm
+    # new_product2v = [[199.99, 500]]
+    # new_product1v = [[199.99]]
+    # new_product2v_svr = scaler.transform([[199.99, 500]])
 
-    single_prediction2v = lm2.predict(new_product2v)
-    single_prediction1v = lm1.predict(new_product1v)
+    # single_prediction2v = lm2.predict(new_product2v)
+    # single_prediction1v = lm1.predict(new_product1v)
 
-    single_prediction_rbf = svr_rbf.predict(new_product2v_svr)
-    single_prediction_lin = svr_lin.predict(new_product2v_svr)
-    single_prediction_poly = svr_poly.predict(new_product2v_svr)
+    # single_prediction_rbf = svr_rbf.predict(new_product2v_svr)
+    # single_prediction_lin = svr_lin.predict(new_product2v_svr)
+    # single_prediction_poly = svr_poly.predict(new_product2v_svr)
     
-    cdf_predict = pd.DataFrame([single_prediction2v, single_prediction1v, single_prediction_rbf, single_prediction_lin, single_prediction_poly],
-                       ['LinReg 2v', 'LinReg 1v', 'SVR RBF', 'SVR Linear', 'SVR Poly'], 
-                       columns=["Predictions"])
-    print(cdf_predict)
+    # cdf_predict = pd.DataFrame([single_prediction2v, single_prediction1v, single_prediction_rbf, single_prediction_lin, single_prediction_poly],
+    #                    ['LinReg 2v', 'LinReg 1v', 'SVR RBF', 'SVR Linear', 'SVR Poly'], 
+    #                    columns=["Predictions"])
+    # print(cdf_predict)
     
-    # ==================================================================== # Thử nghiệm độ lệch trên các phương pháp
-    print("Thực hiện thực nghiệm trên dữ liệu")
+    # # ==================================================================== # Thử nghiệm độ lệch trên các phương pháp
+    # print("Thực hiện thực nghiệm trên dữ liệu")
 
-    real_variables_2v = df.loc[df["Retail Price"].isin(list(range(0,301)))][["Wholesale Price", "Total Sold"]].values
-    real_variables_1v = df.loc[df["Retail Price"].isin(list(range(0,301)))]["Wholesale Price"].values
-    real_variables_2v_svr = scaler.transform(real_variables_2v)
-    real_retail_upto300 = df.loc[df["Retail Price"].isin(list(range(0,301)))]["Retail Price"].values
+    # real_variables_2v = df.loc[df["Retail Price"].isin(list(range(0,301)))][["Wholesale Price", "Total Sold"]].values
+    # real_variables_1v = df.loc[df["Retail Price"].isin(list(range(0,301)))]["Wholesale Price"].values
+    # real_variables_2v_svr = scaler.transform(real_variables_2v)
+    # real_retail_upto300 = df.loc[df["Retail Price"].isin(list(range(0,301)))]["Retail Price"].values
 
-    real_variables_1v = real_variables_1v.reshape(-1, 1)
+    # real_variables_1v = real_variables_1v.reshape(-1, 1)
 
-    lin_reg_prediction2v = lm2.predict(real_variables_2v)
-    lin_reg_prediction1v = lm1.predict(real_variables_1v)
-    rbf_prediction = svr_rbf.predict(real_variables_2v_svr)
-    lin_prediction = svr_lin.predict(real_variables_2v_svr)
-    poly_prediction = svr_poly.predict(real_variables_2v_svr)
+    # lin_reg_prediction2v = lm2.predict(real_variables_2v)
+    # lin_reg_prediction1v = lm1.predict(real_variables_1v)
+    # rbf_prediction = svr_rbf.predict(real_variables_2v_svr)
+    # lin_prediction = svr_lin.predict(real_variables_2v_svr)
+    # poly_prediction = svr_poly.predict(real_variables_2v_svr)
 
-    sns_all.regplot(x=real_retail_upto300, y=real_retail_upto300, color='black', scatter=False)
-    sns_all.regplot(x=real_retail_upto300, y=lin_reg_prediction2v, color='blue', scatter=False)
-    sns_all.regplot(x=real_retail_upto300, y=lin_reg_prediction1v, color='yellow', scatter=False)
-    sns_all.regplot(x=real_retail_upto300, y=rbf_prediction, color='red', scatter=False)
-    sns_all.regplot(x=real_retail_upto300, y=lin_prediction, color='orange', scatter=False)
-    sns_all.regplot(x=real_retail_upto300, y=poly_prediction, color='green', scatter=False)
-    plt_all.show() # Hiển thị
+    # sns_all.regplot(x=real_retail_upto300, y=real_retail_upto300, color='black', scatter=False)
+    # sns_all.regplot(x=real_retail_upto300, y=lin_reg_prediction2v, color='blue', scatter=False)
+    # sns_all.regplot(x=real_retail_upto300, y=lin_reg_prediction1v, color='yellow', scatter=False)
+    # sns_all.regplot(x=real_retail_upto300, y=rbf_prediction, color='red', scatter=False)
+    # sns_all.regplot(x=real_retail_upto300, y=lin_prediction, color='orange', scatter=False)
+    # sns_all.regplot(x=real_retail_upto300, y=poly_prediction, color='green', scatter=False)
+    # plt_all.show() # Hiển thị
 
-    # So sánh điểm số hồi quy giữa các phương pháp
-    cdf_all = pd.DataFrame([r2_lin2v, r2_lin1v, r2_rbf, r2_lin, r2_poly],
-                           ['LinReg 2v', 'LinReg 1v', 'SVR RBF', 'SVR Linear', 'SVR Poly'], 
-                           columns=["R2"])
-    print(cdf_all)
-    # kết luận Phương pháp huấn luyện mô hình tốt nhất là SVR Poly
+    # # So sánh điểm số hồi quy giữa các phương pháp
+    # cdf_all = pd.DataFrame([r2_lin2v, r2_lin1v, r2_rbf, r2_lin, r2_poly],
+    #                        ['LinReg 2v', 'LinReg 1v', 'SVR RBF', 'SVR Linear', 'SVR Poly'], 
+    #                        columns=["R2"])
+    # print(cdf_all)
+    # # kết luận Phương pháp huấn luyện mô hình tốt nhất là SVR Poly
